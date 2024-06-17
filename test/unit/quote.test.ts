d45dbaa51afd8a377f10bd9368f63529fa11edc0
skywarth <yigitk.ersoy@gmail.com>
2024-06-18 02:51:01 +0300

    feat: tests on various QuoteCollection methods bugfix: fixed a bug about concurrency and race condition on quoteCollection access in test environment

--- test/unit/quote.test.ts
+++ test/unit/quote.test.ts
@@ -135,24 +135,27 @@ describe.concurrent("Quote Tests", () => {
     describe("QuoteCollection",()=>{
 
         const quoteArray:Array<Quote>=[
-            new Quote('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','foo',Sentiment.Neutral,[{emotion:Emotion.Joy.Happiness,temperature:4}],[]),
-            new Quote('Maecenas nunc mauris, interdum ut dapibus vitae, sollicitudin in arcu.','bar',Sentiment.Negative,[{emotion:Emotion.Sadness.Grief,temperature:2},{emotion:Emotion.Disgust.Contempt,temperature:2}],['brooding','dark']),
-            new Quote('Suspendisse ut porttitor diam. Integer varius elit vitae condimentum iaculis.','baz',Sentiment.Positive,[{emotion:Emotion.Joy.Relief,temperature:3},{emotion:Emotion.Interest.Kindness,temperature:5}],['hopeful']),
+            new Quote('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','foo',Sentiment.Neutral,[{emotion:Emotion.Joy.Happiness,temperature:4}],[]),//4
+            new Quote('Maecenas nunc mauris, interdum ut dapibus vitae, sollicitudin in arcu.','bar',Sentiment.Negative,[{emotion:Emotion.Sadness.Grief,temperature:2},{emotion:Emotion.Disgust.Contempt,temperature:2}],['brooding','dark']),//4
+            new Quote('Curabitur aliquet quam id dui posuere blandit.', 'quote6', Sentiment.Neutral, [{ emotion: Emotion.Surprise.Wonder, temperature: 3 }], ['wonder']),
+            new Quote('Donec sollicitudin molestie malesuada.', 'quote7', Sentiment.Negative, [{ emotion: Emotion.Sadness.Sorrow, temperature: 2 }, { emotion: Emotion.Disgust.Scorn, temperature: 2 }], ['sorrow', 'scorn']),//4
+            new Quote('Suspendisse ut porttitor diam. Integer varius elit vitae condimentum iaculis.','baz',Sentiment.Positive,[{emotion:Emotion.Joy.Relief,temperature:3},{emotion:Emotion.Interest.Kindness,temperature:5}],['hopeful']),//20
         ];
 
-        let quoteCollection:QuoteCollection;
 
-        beforeEach(() => {
-            quoteCollection=new QuoteCollection(quoteArray);
-        });
+        function getQuoteCollectionInstance():QuoteCollection{
+            return new QuoteCollection(quoteArray);
+        }
 
         test("data getter returns the original data", async() => {
+            const quoteCollection=getQuoteCollectionInstance();
             expect(quoteCollection.data).toBeInstanceOf(Array<Quote>);
             expect(quoteCollection.data).toMatchObject(quoteArray);
         });
 
         describe("find()",() => {
             test("Returns the matching quote by slug when it exists", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
                 const slug='foo';
                 const result=quoteCollection.find(slug);
                 expect(result).toBeInstanceOf(Quote);
@@ -160,6 +163,7 @@ describe.concurrent("Quote Tests", () => {
             });
 
             test("Returns undefined when no quote is found", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
                 const slug='invalid';
                 const result=quoteCollection.find(slug);
                 expect(result).not.toBeInstanceOf(Quote);
@@ -170,15 +174,133 @@ describe.concurrent("Quote Tests", () => {
 
         describe("clone()",() => {
             test("Returns a cloned object", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
                 const cloned=quoteCollection.clone();
-                const alteredClone=cloned.clone().filterBySentiment(Sentiment.Neutral);
                 expect(cloned).toBeInstanceOf(QuoteCollection);
                 expect(cloned).toMatchObject(quoteCollection);
                 expect(cloned).toStrictEqual(quoteCollection);
                 expect(cloned.data).toStrictEqual(quoteCollection.data);
                 expect(cloned).not.toBe(quoteCollection);
 
+            });
+
+            test("Clone doesn't affect the original instance", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+                const cloned=quoteCollection.clone();
+                const alteredClone=cloned.clone().filterBySentiment(Sentiment.Neutral);
+
+
                 expect(alteredClone.data).not.toStrictEqual(cloned.data);
+            });
+        });
+
+        describe("orderByEmotionScoreDesc()",() => {
+
+            test("Ordering direction is descending", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+                const contextEmotionMatrix:Emotion.EmotionMatrix=[
+                    {emotion:Emotion.Interest.Kindness,temperature:4},
+                    {emotion:Emotion.Sadness.Sorrow,temperature:2},
+                    {emotion:Emotion.Sadness.Grief,temperature:3},
+                    {emotion:Emotion.Joy.Happiness,temperature:1},
+
+                ];
+
+                quoteCollection.orderByEmotionScoreDesc(contextEmotionMatrix);
+
+                const incorrectSequenceDetected=quoteCollection.data.some(function(quote,index, arr){
+                    return arr[index+1]?.getEmotionScore(contextEmotionMatrix)>quote.getEmotionScore(contextEmotionMatrix);
+                });
+
+                const scoreArray=quoteCollection.data.map((quote) => {quote.getEmotionScore(contextEmotionMatrix)});
+                const scoreArrayOrderedDesc=[...scoreArray].sort().reverse();
+
+                expect(incorrectSequenceDetected).not.toBe(true);
+                expect(scoreArrayOrderedDesc).toStrictEqual(scoreArray);
+
+            });
+
+            test("Mutating state in-place", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+                const originalQuoteCollection=quoteCollection.clone();
+
+
+                const contextEmotionMatrix:Emotion.EmotionMatrix=[
+                    {emotion:Emotion.Interest.Kindness,temperature:4},
+                    {emotion:Emotion.Sadness.Sorrow,temperature:3},
+                    {emotion:Emotion.Sadness.Grief,temperature:2},
+                    {emotion:Emotion.Joy.Happiness,temperature:1},
+
+                ];
+
+
+                quoteCollection.orderByEmotionScoreDesc(contextEmotionMatrix);
+
+                expect(quoteCollection.data.map(x=>x.slug)).not.toStrictEqual(originalQuoteCollection.data.map(x=>x.slug));
+
+            });
+
+        });
+
+
+        describe("filterByEmotionScoreAboveZero()",() => {
+            test("Doesn't contain any Quote that has emotion score above 0", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+
+
+                const contextEmotionMatrix:Emotion.EmotionMatrix=[
+                    {emotion:Emotion.Interest.Kindness,temperature:4},
+                    {emotion:Emotion.Sadness.Sorrow,temperature:3},
+                    {emotion:Emotion.Sadness.Grief,temperature:2},
+                    {emotion:Emotion.Joy.Happiness,temperature:1},
+                ];
+
+
+                quoteCollection.filterByEmotionScoreAboveZero(contextEmotionMatrix);
+
+                expect(quoteCollection.data.some(x=>x.getEmotionScore(contextEmotionMatrix)<=0)).toBe(false);
+
+            });
+
+        });
+
+        describe("filterByEmotions()",() => {
+            test("Resulting Quotes contain all the emotions", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+                const emotions=[Emotion.Sadness.Sorrow,Emotion.Disgust.Scorn];
+                quoteCollection.filterByEmotions(emotions);
+
+                expect(quoteCollection.data.every(x=>x.hasEmotions(emotions))).toBe(true);
+
+            });
+        });
+
+        describe("filterByTags()",() => {
+            test("[REVISIT HERE]", async() => {
+                //TODO: test
+
+            });
+        });
+
+        describe("filterBySentiment()",() => {
+            test("Resulting Quotes has the sentiment", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+                const sentiment=Sentiment.Neutral;
+                quoteCollection.filterBySentiment(sentiment);
+
+                expect(quoteCollection.data.every(x=>x.sentiment===sentiment)).toBe(true);
+
+            });
+        });
+
+        describe("filterBySlugs()",() => {
+            test("Resulting Quotes has one of the quote slugs", async() => {
+                const quoteCollection=getQuoteCollectionInstance();
+                const quoteSlugs=['foo','bar'];
+                quoteCollection.filterBySlugs(quoteSlugs);
+
+                expect(quoteCollection.data.some(x=>!quoteSlugs.includes(x.slug))).toBe(false);
+
             });
         });
     });
