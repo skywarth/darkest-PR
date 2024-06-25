cd8e1dcd8abfcde2a8505fb41b26655c2fdbe0ff
skywarth <yigitk.ersoy@gmail.com>
2024-06-25 03:03:43 +0300

    refactor: restructuring and refactoring

--- test/unit/config/repositoryConfig.test.ts
+++ test/unit/config/repositoryConfig.test.ts
@@ -1,8 +1,8 @@
-import {afterEach, beforeAll, describe, expect, test, vi} from "vitest";
+import {afterEach, beforeAll, describe, expect, Mock, MockInstance, test, vi} from "vitest";
 import {RepositoryConfig} from "../../../src/Config/RepositoryConfig";
 import nock from "nock";
 import {Context} from "probot";
-import { RequestError } from "@octokit/request-error";
+import {RequestError} from "@octokit/request-error";
 
 
 
@@ -11,48 +11,66 @@ describe('RepositoryConfig', () => {
 
 
     // Mock Context
-    const mockGitHubContext={//TODO: move to initialization, along with spies
-        octokit: {
-            repos: {
-                getContent:vi.fn()
+    let mockGitHubContext:Context;
+    let mockHttpError:Mock;
+    let mockConfigEndpointResponse:Mock;
+    let getContentsSpy:MockInstance;
+    let logErrorSpy:MockInstance;
+
+    const initializeMocks = () => {
+        mockGitHubContext={
+            octokit: {
+                repos: {
+                    getContent:vi.fn()
+                }
+            },
+            repo: () => ({ owner: 'test-owner', repo: 'test-repo' }),
+            log: {
+                error: vi.fn()
             }
-        },
-        repo: () => ({ owner: 'test-owner', repo: 'test-repo' }),
-        log: {
-            error: vi.fn()
-        }
-    } as unknown as Context
+        } as unknown as Context;
 
-    const mockHttpError=(msg:string,statusCode:number)=>{
-        throw new RequestError(msg,statusCode,{
-            request: {
-                method: "POST",
-                url: "https://api.github.com/foo",
-                body: {
-                    bar: "baz",
+        getContentsSpy=vi.spyOn(mockGitHubContext.octokit.repos,'getContent');
+        logErrorSpy=vi.spyOn(mockGitHubContext.log,'error');
+        mockHttpError=vi.fn().mockImplementation((msg:string,statusCode:number):Error=>{
+            return new RequestError(msg,statusCode,{
+                request: {
+                    method: "POST",
+                    url: "https://api.github.com/foo",
+                    body: {
+                        bar: "baz",
+                    },
+                    headers: {
+                        authorization: "token secret123",
+                    },
                 },
-                headers: {
-                    authorization: "token secret123",
+                response: {
+                    status: 500,
+                    url: "https://api.github.com/foo",
+                    headers: {
+                        "x-github-request-id": "1:2:3:4",
+                    },
+                    data: {
+                        foo: "bar",
+                    },
                 },
-            },
-            response: {
-                status: 500,
-                url: "https://api.github.com/foo",
-                headers: {
-                    "x-github-request-id": "1:2:3:4",
-                },
-                data: {
-                    foo: "bar",
-                },
-            },
+            });
         });
-    }
 
-    const getContentsSpy=vi.spyOn(mockGitHubContext.octokit.repos,'getContent');
-    const logErrorSpy=vi.spyOn(mockGitHubContext.log,'error');
+        mockConfigEndpointResponse=vi.fn().mockImplementation((mockConfig:object)=>{
+            return {
+                data: {
+                    content: Buffer.from(JSON.stringify(mockConfig)).toString('base64')
+                }
+            } as any
+        })
+    };
+
+
 
     beforeAll(() => {
         nock.disableNetConnect();
+        initializeMocks();
     });
 
     afterEach(() => {
@@ -60,44 +78,41 @@ describe('RepositoryConfig', () => {
         vi.clearAllMocks();
     });
 
-    test("Reading repository configuration file", async () => {
-        const mockConfig = {
-            debug_mode: true,
-            emojis: false,
-            event_subscriptions: {
-                'pull_request.opened': true
-            }
-        };
-
-        getContentsSpy.mockImplementationOnce(async ()=>{
-            return {
-                data: {
-                    content: Buffer.from(JSON.stringify(mockConfig)).toString('base64')
+    describe('readConfigFromRepository()', () => {
+        test("Reading repository configuration file", async () => {
+            const mockConfig = {
+                debug_mode: true,
+                emojis: false,
+                event_subscriptions: {
+                    'pull_request.opened': true
                 }
-            } as any
+            };
+
+            getContentsSpy.mockImplementationOnce(async ()=>mockConfigEndpointResponse(mockConfig));
+
+            const config = await RepositoryConfig.readConfigFromRepository(mockGitHubContext);
+
+            expect(getContentsSpy).toHaveBeenCalledOnce();
+            expect(config).toBeTypeOf('object');
+            expect(config).toStrictEqual(mockConfig);
+            expect(logErrorSpy).not.toBeCalled();
         });
 
-        const config = await RepositoryConfig.readConfigFromRepository(mockGitHubContext);
+        test("Missing repository configuration file", async () => {
 
-        expect(getContentsSpy).toHaveBeenCalledOnce();
-        expect(config).toBeTypeOf('object');
-        expect(config).toStrictEqual(mockConfig);
-        expect(logErrorSpy).not.toBeCalled();
-    });
+            getContentsSpy.mockRejectedValueOnce(mockHttpError('Not found',404));
 
-    test("Missing repository configuration file", async () => {
+            const config = await RepositoryConfig.readConfigFromRepository(mockGitHubContext);
 
-        getContentsSpy.mockImplementationOnce(async()=>mockHttpError('Not found',404));
+            expect(getContentsSpy).toHaveBeenCalledOnce();
+            expect(config).toBeTypeOf('object');
+            expect(config).toStrictEqual({});
+            expect(logErrorSpy).toHaveBeenCalledOnce();
+            expect(logErrorSpy.mock.calls[0][0]).toBeInstanceOf(Error);
+            expect((logErrorSpy.mock.calls[0][0] as any).status).toBe(404);
 
-        const config = await RepositoryConfig.readConfigFromRepository(mockGitHubContext);
+        });
+    })
 
-        expect(getContentsSpy).toHaveBeenCalledOnce();
-        expect(config).toBeTypeOf('object');
-        expect(config).toStrictEqual({});
-        expect(logErrorSpy).toHaveBeenCalledOnce();
-        expect(logErrorSpy.mock.calls[0][0]).toBeInstanceOf(Error);
-        expect((logErrorSpy.mock.calls[0][0] as any).status).toBe(404);
-
-    });
 
 });
