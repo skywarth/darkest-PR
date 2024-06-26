9131b2b8c1b953c372a9a0f8f6b356672e600151
skywarth <yigitk.ersoy@gmail.com>
2024-06-27 02:02:47 +0300

    feat: feature test for PullRequestReviewerAdded event done

---
+++ test/feature/PullRequest/pullRequestReviewerAdded.test.ts
@@ -0,0 +1,44 @@
+import {describe, test, afterEach, vi, beforeAll} from "vitest";
+import {CaseSlugs} from "../../../src/enums/CaseSlug";
+import {StrategyTestSetup} from "../strategyTestSetup";
+import PullRequestReviewerAdded from "../../../src/ActionHandler/PullRequest/PullRequestReviewerAdded";
+
+
+import pullRequestReviewerAddedPayload from '../../fixtures/events/pull_request/pull_request.reviewer.added.json';
+
+
+describe("Pull Request Opened Tests", () => {
+    const strategyTestSetup = new StrategyTestSetup();
+
+    beforeAll(() => {
+        strategyTestSetup.beforeAll();
+    });
+
+    afterEach(() => {
+        strategyTestSetup.afterEach();
+    });
+
+    describe.each([
+        {
+            description: "Reviewer added",
+            expectedCaseSlug: CaseSlugs.PullRequest.Reviewer.Added,
+        }
+    ])('$description', ({ expectedCaseSlug }) => {
+        test.only('Creates a comment after receiving the event', async () => {
+
+            strategyTestSetup.actionStrategyHandleSpy = vi.spyOn(PullRequestReviewerAdded.prototype as any, 'handle');
+
+
+            await strategyTestSetup.probot.receive({
+                id: '123',
+                name: 'pull_request',
+                payload: pullRequestReviewerAddedPayload as any,
+            });
+
+
+            strategyTestSetup.performCommonAssertions(expectedCaseSlug);
+
+
+        });
+    });
+});
