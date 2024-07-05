b957e98f797c841eedc5be2070ade4222e90dc55
skywarth <yigitk.ersoy@gmail.com>
2024-07-05 03:34:36 +0300

    trigger deploy #2

--- api/github/webhooks/serverless.ts
+++ api/github/webhooks/serverless.ts
@@ -1,6 +1,7 @@
 import {createNodeMiddleware, createProbot} from "probot";
 import app from "../../../src/index.js";
 
+
 const probot = createProbot();
 
 export default createNodeMiddleware(app, {
